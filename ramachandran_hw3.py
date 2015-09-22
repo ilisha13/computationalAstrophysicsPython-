18257d0752a8b1b889835cd6d191d336c9777132
Ilisha Ramachandran <ilisha@Ilishas-Mac.local>
2015-09-22 11:23:38 -0400

    Final upload of homework 3

--- ramachandran_hw3.py
+++ ramachandran_hw3.py
@@ -3,11 +3,13 @@ from matplotlib.pyplot import *
 import numpy as np 
 from math import *
 
+
+#Imports the model values
 with open ('model_smg.dat') as file_name:
  
     x, y = np.float64(np.loadtxt(file_name, skiprows = 0, unpack=True))
     
-
+#Imports the observed values 
 with open ('ncounts_850.dat') as file_name:
     
     x1,y1 = np.float64(np.loadtxt(file_name, skiprows = 0, unpack=True))
@@ -15,20 +17,34 @@ with open ('ncounts_850.dat') as file_name:
     figure(0)
     plot(x1,y1,'ro')
     
-    
+#The equation for finding dN/dL from the model values. Returns dN/dL    
 def f_prime(x,y):
     h1 = x[i]-x[i-1]
     h2 = x[i+1]-x[i]
     return -(((h1/(h2*(h1+h2)))*(y[i+1])) - (((h1-h2)/(h2*h1))*(y[i])) - ((h2/(h1*(h1+h2)))*(y[i-1])))
-    
+
+
+#Makes a list of dN/dL for the values of x that does not include first and last value. Also log10 values.
+model_dN_dL = list()
+
 for i in range (1,11):
     y2 = f_prime(x,y)
     print (y2)
-    
-    
-    figure(1)
-    plot(x1,y1,'ro')
-    plot(log10(x[i]),log10(y2),'bo')
-    xlabel("Luminosity")
-    ylabel("Number of galexies above luminoisty")
-    title("Graph of model data") 
+    log_lumi = np.log10(y2)
+    model_dN_dL.append(log_lumi)
+
+#Logs the luminosity values and cuts off first and last value 
+
+lumi = np.log10(x[1:-1])
+
+#Plots model data vs observed data 
+
+figure(0)
+plot(x1,y1,'ro',label = "Observed Data")
+plot(lumi,model_dN_dL,'bo',label = "Model Data")
+xlabel("Log of Luminosity")
+ylabel("Log of Number of galexies above luminoisty")
+title("Graph of Model Data vs Observed Data")
+legend(numpoints=1)
+show()
+savefig()
