2fc1a54dc1be7c1d0a5233eeba8a332b007ea012
Ilisha Ramachandran <iramachand@brynmawr.edu>
2015-10-21 06:14:02 +0000

    ramachandran_hw6.py edited online with Bitbucket

--- ramachandran_hw6.py
+++ ramachandran_hw6.py
@@ -1,24 +1,33 @@
 from datetime import datetime
 from datetime import timedelta
+from math import exp
+import numpy as np
 
-t1 = datetime.now()
-t2 = datetime.now()
 
 # Derivative function
-def f_prime(x,y):
+"""def f_prime(x,y):
     h1 = x[i]-x[i-1]
     h2 = x[i+1]-x[i]
-    return -(((h1/(h2*(h1+h2)))*(y[i+1])) - (((h1-h2)/(h2*h1))*(y[i])) - ((h2/(h1*(h1+h2)))*(y[i-1])))
+    return -(((h1/(h2*(h1+h2)))*(y[i+1])) - (((h1-h2)/(h2*h1))*(y[i])) - ((h2/(h1*(h1+h2)))*(y[i-1])))"""
+
+def f_prime(y):
+    def derivative(x, h=.1e-5):
+        return( (y(x+h/2)-y(x-h/2))/h)
+    return derivative 
+
 
 #Black body function to test 
 
 def B(T):
     h = 6.626 * 10**(-27)
     c = 2.99 * 10**(10)
-    v = 
+    wavelength = 0.087
+    v = c/wavelength
     k = 1.380658 * 10**(-16)
 
-def Newton(x0, f, f_prime, tol = 0.001, N = 300):
+    return (((2.0 * h * (v**3))/c**2)/(exp((h*v)/(k*T))-1.0)  - 1.25 * 10**(-12))
+
+def Newton(x0, f, f_prime, tol = 1*10**(-15), N = 300):
 
     n = 1
 
@@ -33,6 +42,61 @@ def Newton(x0, f, f_prime, tol = 0.001, N = 300):
         else:
             x0 = xi
 
-    n = n+1 
+        #n += 1 
+
+def bisection (f, a, b, i, tolerance) :
+    
+    fa = f(a)
+    fb = f(b)
+    
+    if (fa * fb) > 0:
+        print("a and b are not bracketing the roots")
+
+        a = a * 1/2
+        b = b * 2
+                
+    else:
+
+        #print("a and b are bracketing the roots")
+
+        i = 1 
+        
+        while (b-a)/2. > tolerance: 
+
+            c = (b+a)/2.
+            
+            fc = f(c)
+
+            if 0 <= fc <= tolerance:
+                return c
+            
+            elif fc * fa < 0:
+                b = c
+              
+            else:
+                a = c
+               
+            i += 1
+
+        return c        
+
+def g(x):
+    return (x**3) - x - 2
+def y(x):
+    return -6 + x + (x**2)
+
+print("-----------------------------------------------------------")
+
+print("This is to test the Newton function works")
+print ("The root for g(x) is", bisection (g, 1, 2, 30, 0.001))
+print("The root for g(x) by netwon is", Newton(6,g,f_prime(g)))
+print ("The root for y(x) is", bisection (y, 0, 5, 30, 0.001))
+print("The root for y(x) by netwon is", Newton(6,y,f_prime(y)))
+
+print("-----------------------------------------------------------")
+
+print ("The root for B(T) is", bisection (B,10, 80, 100, 1*10**(-15)))
+print("The root for B(T) by netwon is", Newton(80,B,f_prime(B)))
+
 
         
